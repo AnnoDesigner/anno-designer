f7363127b515de27ec408b93279e481664b30d3a
Agmas <g999oldy@gmail.com>
2019-03-30 14:22:50 +0000

    group objects by Anno version, 2205 buildings include modules
    Grouped buildings and factions by Anno version. Each 2205 building now includes its modules as a sibling list, rather than in a separate "Facility Modules" tab on the tree view.

--- AnnoDesigner/Presets/BuildingPresets.cs
+++ AnnoDesigner/Presets/BuildingPresets.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -21,22 +22,80 @@ namespace AnnoDesigner.Presets
         public void AddToTree(TreeView treeView)
         {
             var excludedTemplates = new[] { "Ark", "Harbour", "OrnamentBuilding" };
-            var excludedFactions = new[] { "third party" };
-            var list = Buildings.Where(_ => !excludedTemplates.Contains(_.Template)).Where(_ => !excludedFactions.Contains(_.Faction));
-            foreach (var firstLevel in list.GroupBy(_ => _.Faction).OrderBy(_ => _.Key))
+            var excludedFactions = new[] { "third party", "Facility Modules" };
+            var list = Buildings
+                .Where(_ => !excludedTemplates.Contains(_.Template))
+                .Where(_ => !excludedFactions.Contains(_.Faction));
+
+            //For Anno 2205 only
+            var modulesList = Buildings
+                            .Where(_ => _.Header == "Anno 2205")
+                            .Where(_ => _.Faction == "Facility Modules")
+                            .Where(_ => _.Faction != "Facilities")
+                            .ToList();
+            var facilityList = list.Where(_ => _.Faction == "Facilities").ToList();
+                //Get a list of nonMatchedModules;
+            var nonMatchedModulesList = modulesList.Except(facilityList, new BuildingInfoModuleComparer()).ToList();
+            //These appear to all match. The below statement should notify the progammer if we need to add handling for non matching lists
+            System.Diagnostics.Debug.Assert(nonMatchedModulesList.Count == 0, "Module lists do not match, implement handling for this");
+
+            foreach (var header in list.GroupBy(_ => _.Header).OrderBy(_ => _.Key))
             {
-                var firstLevelItem = new TreeViewItem { Header = firstLevel.Key };
-                foreach (var secondLevel in firstLevel.GroupBy(_ => _.Group).OrderBy(_ => _.Key))
+                var headerItem = new TreeViewItem { Header = header.Key };
+                foreach (var secondLevel in list.GroupBy(_ => _.Faction).OrderBy(_ => _.Key))
                 {
                     var secondLevelItem = new TreeViewItem { Header = secondLevel.Key };
-                    foreach (var buildingInfo in secondLevel.OrderBy(_ => _.GetOrderParameter()))
+                    foreach (var thirdLevel in secondLevel.GroupBy(_ => _.Group).OrderBy(_ => _.Key))
                     {
-                        secondLevelItem.Items.Add(buildingInfo.ToAnnoObject());
+                        var thirdLevelItem = new TreeViewItem { Header = thirdLevel.Key };
+                        foreach (var buildingInfo in thirdLevel.OrderBy(_ => _.GetOrderParameter()))
+                        {
+                            thirdLevelItem.Items.Add(buildingInfo.ToAnnoObject());
+                        }
+                        //For 2205 only
+                        //Add building modules to element list.
+                        //Group will be the same for elements in the list.
+                        var fourthLevelItem = new TreeViewItem { Header = thirdLevel.ElementAt(0).Group + " Modules" };
+                        foreach (var fourthLevel in modulesList
+                            .Where(_ => _.Group == thirdLevel.ElementAt(0).Group))
+                        {
+                            fourthLevelItem.Items.Add(fourthLevel.ToAnnoObject());
+                        }
+                        if (fourthLevelItem.Items.Count > 0)
+                        {
+                            thirdLevelItem.Items.Add(fourthLevelItem);
+                        }
+
+                        secondLevelItem.Items.Add(thirdLevelItem);
                     }
-                    firstLevelItem.Items.Add(secondLevelItem);
+                    headerItem.Items.Add(secondLevelItem);
                 }
-                treeView.Items.Add(firstLevelItem);
+                treeView.Items.Add(headerItem);
             }
+        }
+    }
+
+    /// <summary>
+    /// Comparer used to check if two BuildingInfo groups match
+    /// </summary>
+    public class BuildingInfoModuleComparer : IEqualityComparer<BuildingInfo>
+    {
+        public bool Equals(BuildingInfo x, BuildingInfo y)
+        {
+
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the buildingInfo group properties are equal
+            return x.Group == y.Group;
+        }
+        public int GetHashCode(BuildingInfo obj)
+        {
+            return base.GetHashCode();
         }
     }
 }
