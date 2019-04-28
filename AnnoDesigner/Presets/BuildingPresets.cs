846bcc38bcb23e133a888526c52fef91e2b4634d
FroggieFrog <FroggieFrog@users.noreply.github.com>
2019-04-28 14:09:46 +0200

    write version at beginning of presets file (#26)

--- AnnoDesigner/Presets/BuildingPresets.cs
+++ AnnoDesigner/Presets/BuildingPresets.cs
@@ -13,10 +13,10 @@ namespace AnnoDesigner.Presets
     [DataContract]
     public class BuildingPresets
     {
-        [DataMember]
+        [DataMember(Order = 0)]
         public string Version;
 
-        [DataMember]
+        [DataMember(Order = 1)]
         public List<BuildingInfo> Buildings;
 
         public void AddToTree(TreeView treeView)
@@ -34,7 +34,7 @@ namespace AnnoDesigner.Presets
                             .Where(_ => _.Faction != "Facilities")
                             .ToList();
             var facilityList = list.Where(_ => _.Faction == "Facilities").ToList();
-                //Get a list of nonMatchedModules;
+            //Get a list of nonMatchedModules;
             var nonMatchedModulesList = modulesList.Except(facilityList, new BuildingInfoModuleComparer()).ToList();
             //These appear to all match. The below statement should notify the progammer if we need to add handling for non matching lists
             System.Diagnostics.Debug.Assert(nonMatchedModulesList.Count == 0, "Module lists do not match, implement handling for this");
@@ -45,7 +45,7 @@ namespace AnnoDesigner.Presets
                 foreach (var secondLevel in header.GroupBy(_ => _.Faction).OrderBy(_ => _.Key))
                 {
                     var secondLevelItem = new TreeViewItem { Header = secondLevel.Key };
-                  
+
                     foreach (var thirdLevel in secondLevel.Where(_ => _.Group != null).GroupBy(_ => _.Group).OrderBy(_ => _.Key))
                     {
                         var thirdLevelItem = new TreeViewItem { Header = thirdLevel.Key };
