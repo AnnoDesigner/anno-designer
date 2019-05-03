2779be16078d03574550062719726351631464ef
FroggieFrog <FroggieFrog@users.noreply.github.com>
2019-05-03 13:42:49 +0200

    - better memory management when loading icons (Directory.GetFiles vs Directory.EnumerateFiles) - some goodies for debugging

--- AnnoDesigner/UI/IconImage.xaml.cs
+++ AnnoDesigner/UI/IconImage.xaml.cs
@@ -1,43 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows.Media.Imaging;
 
 namespace AnnoDesigner.UI
 {
+    [DebuggerDisplay("{Name}")]
     public class IconImage
     {
         private readonly Dictionary<string, string> _localizations;
 
-        public BitmapImage Icon
-        {
-            get;
-            private set;
-        }
-
-        public string Name
-        {
-            get;
-            private set;
-        }
-
-        public string DisplayName
-        {
-            get
-            {
-                return _localizations == null ? Name : _localizations["eng"];
-            }
-        }
+        #region ctor
 
         public IconImage(string name)
         {
-            _localizations = null;
             Name = name;
+            _localizations = null;
         }
 
-        public IconImage(string name, Dictionary<string,string> localizations, BitmapImage icon)
+        public IconImage(string name, Dictionary<string, string> localizations, BitmapImage icon) : this(name)
         {
             _localizations = localizations;
-            Name = name;
             Icon = icon;
         }
+
+        #endregion        
+
+        public string Name { get; }
+
+        public string DisplayName
+        {
+            get { return _localizations == null ? Name : _localizations["eng"]; }
+        }
+
+        public BitmapImage Icon { get; }
     }
 }
