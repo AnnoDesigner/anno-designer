cbf1892824e89f61726b96c0e1deaf9dea01a9e4
Agmas <g999oldy@gmail.com>
2019-05-04 15:37:23 +0100

    fix casing, influence type not updating correctly on language change

--- AnnoDesigner/MainWindow.xaml.cs
+++ AnnoDesigner/MainWindow.xaml.cs
@@ -23,8 +23,7 @@ namespace AnnoDesigner
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
-        : Window
+    public partial class MainWindow : Window
     {
         private readonly WebClient _webClient;
         private IconImage _noIconItem;
@@ -52,12 +51,12 @@ namespace AnnoDesigner
             }
         }
 
-        private static Localization.MainWindow mainWindowLocalization;
+        private static Localization.MainWindow _mainWindowLocalization;
         //About window does not need to be called, as it get's instantiated and used when the about window is created
 
         private void SelectedLanguageChanged()
         {
-            mainWindowLocalization.UpdateLanguage();
+            _mainWindowLocalization.UpdateLanguage();
             _instance.RepopulateTreeView();
             foreach (MenuItem item in LanguageMenu.Items)
             {
@@ -78,7 +77,7 @@ namespace AnnoDesigner
 
             foreach (string rangeType in rangeTypes)
             {
-                comboxBoxInfluenceType.Items.Add(Localization.Localization.Translations[language][rangeType]);
+                comboxBoxInfluenceType.Items.Add(new KeyValuePair<BuildingInfluenceType, string>((BuildingInfluenceType)Enum.Parse(typeof(BuildingInfluenceType), rangeType), Localization.Localization.Translations[language][rangeType]));
             }
             comboxBoxInfluenceType.SelectedIndex = 0;
 
@@ -103,7 +102,7 @@ namespace AnnoDesigner
 
             //Get a reference an instance of Localization.MainWindow, so we can call UpdateLanguage() in the SelectedLanguage setter
             DependencyObject dependencyObject = LogicalTreeHelper.FindLogicalNode(this, "Menu");
-            mainWindowLocalization = (Localization.MainWindow)((Menu)dependencyObject).DataContext;
+            _mainWindowLocalization = (Localization.MainWindow)((Menu)dependencyObject).DataContext;
 
             //If language is not recognized, bring up the language selection screen
             if (!Localization.Localization.LanguageCodeMap.ContainsKey(Settings.Default.SelectedLanguage))
@@ -373,7 +372,7 @@ namespace AnnoDesigner
             AnnoObject obj = new AnnoObject
             {
                 Size = new Size(textBoxWidth?.Value ?? 1, textBoxHeight?.Value ?? 1),
-                Color = colorPicker.SelectedColor.HasValue ? colorPicker.SelectedColor.Value : Colors.Red,
+                Color = colorPicker.SelectedColor ?? Colors.Red,
                 Label = IsChecked(checkBoxLabel) ? textBoxLabel.Text : "",
                 Icon = comboBoxIcon.SelectedItem == _noIconItem ? null : ((IconImage)comboBoxIcon.SelectedItem).Name,
                 Radius = textBoxRadius?.Value ?? 0,
@@ -403,7 +402,7 @@ namespace AnnoDesigner
                 {
                     UpdateUIFromObject(new AnnoObject(selectedItem)
                     {
-                        Color = colorPicker.SelectedColor.HasValue ? colorPicker.SelectedColor.Value : Colors.Red,
+                        Color = colorPicker.SelectedColor ?? Colors.Red,
                     });
                     ApplyCurrentObject();
                 }
@@ -515,7 +514,7 @@ namespace AnnoDesigner
             // registers the .ad file extension to the anno_designer class
             Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\.ad", null, "anno_designer");
 
-            showRegistrationMessageBox(isDeregistration: false);
+            ShowRegistrationMessageBox(isDeregistration: false);
         }
 
         private void MenuItemUnregisterExtensionClick(object sender, RoutedEventArgs e)
@@ -527,11 +526,11 @@ namespace AnnoDesigner
                 Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\anno_designer");
                 Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\.ad");
 
-                showRegistrationMessageBox(isDeregistration: true);
+                ShowRegistrationMessageBox(isDeregistration: true);
             }
         }
 
-        private void showRegistrationMessageBox(bool isDeregistration)
+        private void ShowRegistrationMessageBox(bool isDeregistration)
         {
             string language = AnnoDesigner.Localization.Localization.GetLanguageCodeFromName(SelectedLanguage);
             var message = isDeregistration ? AnnoDesigner.Localization.Localization.Translations[language]["UnregisterFileExtensionSuccessful"] : AnnoDesigner.Localization.Localization.Translations[language]["RegisterFileExtensionSuccessful"];
