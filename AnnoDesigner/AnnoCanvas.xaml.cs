e75babeefa6eb3d4eba85c0374fde50c1a9325f1
Agmas <g999oldy@gmail.com>
2019-05-08 19:12:34 +0100

    require copy/paste to use control key, add IsShiftPressed()

--- AnnoDesigner/AnnoCanvas.xaml.cs
+++ AnnoDesigner/AnnoCanvas.xaml.cs
@@ -1271,7 +1271,7 @@ namespace AnnoDesigner
                     // user clicked nothing: start dragging the selection rect
                     CurrentMode = MouseMode.SelectionRectStart;
                 }
-                else if (!IsControlPressed())
+                else if (!(IsControlPressed() || IsShiftPressed()))
                 {
                     CurrentMode = _selectedObjects.Contains(obj) ? MouseMode.DragSelectionStart : MouseMode.DragSingleStart;
                 }
@@ -1337,7 +1337,7 @@ namespace AnnoDesigner
                     switch (CurrentMode)
                     {
                         case MouseMode.SelectionRect:
-                            if (IsControlPressed())
+                            if ((IsControlPressed() || IsShiftPressed()))
                             {
                                 // remove previously selected by the selection rect
                                 _selectedObjects.RemoveAll(_ => GetObjectScreenRect(_).IntersectsWith(_selectionRect));
@@ -1415,7 +1415,7 @@ namespace AnnoDesigner
                 {
                     default:
                         // clear selection if no key is pressed
-                        if (!IsControlPressed())
+                        if (!(IsControlPressed() || IsShiftPressed()))
                         {
                             _selectedObjects.Clear();
                         }
@@ -1455,7 +1455,7 @@ namespace AnnoDesigner
                             var obj = GetObjectAt(_mousePosition);
                             if (obj == null)
                             {
-                                if (!IsControlPressed())
+                                if (!(IsControlPressed() || IsShiftPressed()))
                                 {
                                     // clear selection
                                     _selectedObjects.Clear();
@@ -1507,16 +1507,22 @@ namespace AnnoDesigner
                     _selectedObjects.Clear();
                     break;
                 case Key.C:
-                    if (_selectedObjects.Count != 0)
+                    if (IsControlPressed())
                     {
-                        ObjectClipboard = CloneList(_selectedObjects);
+                        if (_selectedObjects.Count != 0)
+                        {
+                            ObjectClipboard = CloneList(_selectedObjects);
+                        }
                     }
                     break;
                 case Key.V:
-                    if (ObjectClipboard.Count != 0)
+                    if (IsControlPressed())
                     {
-                        CurrentObjects = CloneList(ObjectClipboard);
-                        MoveCurrentObjectsToMouse();
+                        if (ObjectClipboard.Count != 0)
+                        {
+                            CurrentObjects = CloneList(ObjectClipboard);
+                            MoveCurrentObjectsToMouse();
+                        }
                     }
                     break;
                 case Key.R:
@@ -1542,12 +1548,21 @@ namespace AnnoDesigner
         }
 
         /// <summary>
-        /// Checks whether the user is pressing keys to signal that he wants to select multiple objects
+        /// Checks whether the user is pressing the control key.
         /// </summary>
         /// <returns></returns>
         private static bool IsControlPressed()
         {
             return Keyboard.Modifiers.HasFlag(ModifierKeys.Control) || Keyboard.Modifiers.HasFlag(ModifierKeys.Shift);
+        }
+
+        /// <summary>
+        /// Checks whether the user is pressing the shift key.
+        /// </summary>
+        /// <returns></returns>
+        private static bool IsShiftPressed()
+        {
+            return Keyboard.Modifiers.HasFlag(ModifierKeys.Shift);
         }
 
         #endregion
