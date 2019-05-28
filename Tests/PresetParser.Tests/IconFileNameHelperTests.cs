98fbeed02b0f5d386fc3bbe86f06e84f3fcbd796
FroggieFrog <FroggieFrog@users.noreply.github.com>
2019-05-28 08:35:00 +0200

    add tests for IconFileNameHelper

--- Tests/PresetParser.Tests/IconFileNameHelperTests.cs
+++ Tests/PresetParser.Tests/IconFileNameHelperTests.cs
@@ -11,24 +11,88 @@ namespace PresetParser.Tests
     public class IconFileNameHelperTests
     {
         [Fact]
-        public void GetIconFilename_AnnoVersionIs1404_ShouldReturnFileNameWithIndex()
+        public void GetIconFilename_AnnoVersionIs1404_ShouldReturnFileNameWithPrefix()
         {
-            //// Arrange
-            //var helper = new IconFileNameHelper();
+            // Arrange
+            var helper = new IconFileNameHelper();
 
-            //var doc = new XmlDocument();
-            //var rootNode = doc.CreateNode(XmlNodeType.Element, "IconFileID", "");
+            var doc = new XmlDocument();
+            doc.LoadXml("<root><IconFileID>myFileId</IconFileID><IconIndex>42</IconIndex></root>");
+            var rootNode = doc["root"];
 
-            //var iconNode = doc.CreateNode(XmlNodeType.Text, "IconFileID", "");
-            //rootNode.InnerText = "inner";
+            // Act
+            var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_1404);
 
-            //rootNode.AppendChild(iconNode);
+            // Assert
+            Assert.StartsWith("A4_", result);
+        }
 
-            //// Act
-            //var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_1404);
+        [Fact]
+        public void GetIconFilename_AnnoVersionIs1404AndNoIconIndex_ShouldReturnFileNameWithPrefixAndIconIndexZero()
+        {
+            // Arrange
+            var helper = new IconFileNameHelper();
 
-            //// Assert
-            //Assert.StartsWith("A4_", result);
+            var doc = new XmlDocument();
+            doc.LoadXml("<root><IconFileID>myFileId</IconFileID></root>");
+            var rootNode = doc["root"];
+
+            // Act
+            var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_1404);
+
+            // Assert
+            Assert.Equal("A4_icon_myFileId_0.png", result);
+        }
+
+        [Fact]
+        public void GetIconFilename_AnnoVersionIs1404AndIconIndex_ShouldReturnFileNameWithPrefixAndIconIndex()
+        {
+            // Arrange
+            var helper = new IconFileNameHelper();
+
+            var doc = new XmlDocument();
+            doc.LoadXml("<root><IconFileID>myFileId</IconFileID><IconIndex>42</IconIndex></root>");
+            var rootNode = doc["root"];
+
+            // Act
+            var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_1404);
+
+            // Assert
+            Assert.Equal("A4_icon_myFileId_42.png", result);
+        }
+
+        [Fact]
+        public void GetIconFilename_AnnoVersionIsNot1404AndNoIconIndex_ShouldReturnFileNameWithIconIndexZero()
+        {
+            // Arrange
+            var helper = new IconFileNameHelper();
+
+            var doc = new XmlDocument();
+            doc.LoadXml("<root><IconFileID>myFileId</IconFileID></root>");
+            var rootNode = doc["root"];
+
+            // Act
+            var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_1800);
+
+            // Assert
+            Assert.Equal("icon_myFileId_0.png", result);
+        }
+
+        [Fact]
+        public void GetIconFilename_AnnoVersionIsNot1404AndIconIndex_ShouldReturnFileNameWithIconIndex()
+        {
+            // Arrange
+            var helper = new IconFileNameHelper();
+
+            var doc = new XmlDocument();
+            doc.LoadXml("<root><IconFileID>myFileId</IconFileID><IconIndex>42</IconIndex></root>");
+            var rootNode = doc["root"];
+
+            // Act
+            var result = helper.GetIconFilename(rootNode, Constants.ANNO_VERSION_2205);
+
+            // Assert
+            Assert.Equal("icon_myFileId_42.png", result);
         }
     }
 }
