3a74918b25af77cc048cfe5fdbb3d64ddc9a20cb
FroggieFrog <FroggieFrog@users.noreply.github.com>
2019-05-29 16:09:16 +0200

    add option to use binary logs

--- build/StartCakeBuild.bat
+++ build/StartCakeBuild.bat
@@ -48,12 +48,42 @@ echo not supported MSBUILD_VERSION: %MSBUILD_VERSION%
 goto :MSBUILD_VERSION_PROMPT
 )
 
+@rem get USE_BINARY_LOG
+set "USE_BINARY_LOG=%~1"
+goto :USE_BINARY_LOG_CHECK
+:USE_BINARY_LOG_PROMPT
+set /p "USE_BINARY_LOG=Use binary logs? (Cause problems with paths containing spaces) (no[0] | yes[1]): "
+:USE_BINARY_LOG_CHECK
+if "%USE_BINARY_LOG%"=="" goto :USE_BINARY_LOG_PROMPT
+set USE_BINARY_LOG_RESULT=false
+if "%USE_BINARY_LOG%"=="0" set USE_BINARY_LOG_RESULT=true
+if "%USE_BINARY_LOG%"=="1" set USE_BINARY_LOG_RESULT=true
+if "%USE_BINARY_LOG%"=="0" (
+set USE_BINARY_LOG_RESULT=true
+set USE_BINARY_LOG=0
+)
+if "%USE_BINARY_LOG%"=="1" (
+set USE_BINARY_LOG_RESULT=true
+set USE_BINARY_LOG=1
+)
+if "%USE_BINARY_LOG_RESULT%"=="false" (
+echo not supported USE_BINARY_LOG: %USE_BINARY_LOG%
+goto :USE_BINARY_LOG_PROMPT
+)
+
+set BINARY_LOG=false
+if "%USE_BINARY_LOG%"=="1" (
+set BINARY_LOG=true
+)
+
 echo:
 echo BUILD_MODE=%BUILD_MODE%
 echo MSBUILD_VERSION=%MSBUILD_VERSION%
+echo USE_BINARY_LOG=%BINARY_LOG%
 echo:
 
-Powershell.exe -NoProfile -ExecutionPolicy ByPass -File "%~dp0\build.ps1" -configuration %BUILD_MODE% -msbuildVersion=%MSBUILD_VERSION%
+rem  -Verbosity Diagnostic
+Powershell.exe -NoProfile -ExecutionPolicy ByPass -File "%~dp0\build.ps1" -configuration %BUILD_MODE% -msbuildVersion=%MSBUILD_VERSION% -useBinaryLog=%BINARY_LOG%
 
 :EXIT
 echo finished
