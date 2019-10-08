b636a0d7d5c4bb32f17b4cac8ad7a995eb08d440
StingMcRay <teddybeer@multiweb.nl>
2019-10-08 15:22:20 +0200

    update Presets to 3.2.8 (#145)
    - Preset updated with new Ornaments and Botanica buildings
    - Icons Update for the preset update
    - Missing Land Depot Anno 2070 Fix
    - optimized images with zopfliPng (740KB -> 470 KB)
    - fixed unit test

--- Tests/PresetParser.Tests/ExtraPresetsTests.cs
+++ Tests/PresetParser.Tests/ExtraPresetsTests.cs
@@ -24,8 +24,8 @@ namespace PresetParser.Tests
 
         [Theory]
         [InlineData(Constants.ANNO_VERSION_1404, 4)]
-        [InlineData(Constants.ANNO_VERSION_2070, 4)]
-        [InlineData(Constants.ANNO_VERSION_1800, 23)]
+        [InlineData(Constants.ANNO_VERSION_2070, 5)]
+        [InlineData(Constants.ANNO_VERSION_1800, 24)]
         [InlineData(Constants.ANNO_VERSION_2205, 0)]
         public void GetExtraPresets_AnnoVersionIsKnown_ShouldReturnCorrectExtraPresetsCount(string annoVersion, int expectedCount)
         {
