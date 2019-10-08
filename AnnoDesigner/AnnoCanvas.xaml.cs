655a9aec224742060cb4db24ab431d0cf5712b7d
FroggieFrog <FroggieFrog@users.noreply.github.com>
2019-10-08 13:32:52 +0200

    - fix when DragAll

--- AnnoDesigner/AnnoCanvas.xaml.cs
+++ AnnoDesigner/AnnoCanvas.xaml.cs
@@ -1097,7 +1097,8 @@ namespace AnnoDesigner
                     _mouseDragStart.X += _coordinateHelper.GridToScreen(dx, GridSize);
                     _mouseDragStart.Y += _coordinateHelper.GridToScreen(dy, GridSize);
 
-                    StatisticsUpdated?.Invoke(this, EventArgs.Empty);
+                    //all is moved -> no need to update statistics
+                    //StatisticsUpdated?.Invoke(this, EventArgs.Empty);
                 }
             }
             else if (e.LeftButton == MouseButtonState.Pressed)
