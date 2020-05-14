b51fb58b495b47c7f7c12064d9ac6d986ecff03b
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-05-14 11:42:40 +0200

    add cache for Pens

---
+++ AnnoDesigner/PenCache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using AnnoDesigner.model;
+
+namespace AnnoDesigner
+{
+    public class PenCache : IPenCache
+    {
+        private static readonly Dictionary<(Brush brush, double thickness), Pen> _cachedPens;
+
+        static PenCache()
+        {
+            _cachedPens = new Dictionary<(Brush brush, double thickness), Pen>();
+        }
+
+        public Pen GetPen(Brush brush, double thickness)
+        {
+            if (!_cachedPens.TryGetValue((brush, thickness), out var foundPen))
+            {
+                foundPen = new Pen(brush, thickness);
+                if (foundPen.CanFreeze)
+                {
+                    foundPen.Freeze();
+                }
+
+                _cachedPens.Add((brush, thickness), foundPen);
+            }
+
+            return foundPen;
+        }
+    }
+}
