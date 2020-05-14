3b269d4ad8b6689d35c65cf5ec19ce068cf2c33d
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-05-14 21:36:08 +0200

    add first draft

---
+++ AnnoDesigner.Core/Models/IRecentFilesSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoDesigner.Core.Models
+{
+    public interface IRecentFilesSerializer
+    {
+        /*
+         * Implementations could serialize to XML or JSON or SQLite or ...
+         * I would prefer JSON in the "AppData\Local\AnnoDesigner" directory. The app-settings are already there.
+         */
+
+        /// <summary>
+        /// Serializes the list of recently used files to a file.
+        /// </summary>
+        /// <param name="path">The path to the file with the recently used files.</param>
+        /// <param name="recentFiles">The list of recently used files.</param>
+        void Serialize(string path, List<RecentFile> recentFiles);
+
+        /// <summary>
+        /// Loads a list of recently used files from a file.
+        /// </summary>
+        /// <param name="path">The path to the file with the recently used files.</param>
+        /// <returns>The list of recently used files.</returns>
+        List<RecentFile> Deserialize(string path);
+    }
+}
---
+++ AnnoDesigner.Core/Models/RecentFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoDesigner.Core.Models
+{
+    public class RecentFile
+    {
+        public RecentFile(string pathToUse, DateTime lastUsedToUse)
+        {
+            Path = pathToUse;
+            LastUsed = lastUsedToUse;
+        }
+
+        /// <summary>
+        /// The path to the file.
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// The last time the file was loaded/used.
+        /// </summary>
+        public DateTime LastUsed { get; set; }
+    }
+}
