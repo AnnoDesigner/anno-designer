bd1f1bed450aa36976396e6ce276aaf9ed43f3c0
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-05-15 19:08:44 +0200

    fix references

--- FandomParser/FandomParser.Core/Presets/Models/WikiBuildingInfo.cs
+++ FandomParser/FandomParser.Core/Presets/Models/WikiBuildingInfo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.Serialization;
 
 namespace FandomParser.Core.Presets.Models
