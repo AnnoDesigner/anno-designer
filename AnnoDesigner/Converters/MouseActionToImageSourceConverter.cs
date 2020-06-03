1f57645b0e46fb71f3ced7a5a87ead55a07b9c1f
AgmasGold <48861520+AgmasGold@users.noreply.github.com>
2020-06-03 19:54:22 +0100

    Remove left over ImageSourceConverter code

--- AnnoDesigner/Converters/MouseActionToImageSourceConverter.cs
+++ AnnoDesigner/Converters/MouseActionToImageSourceConverter.cs
@@ -27,20 +27,16 @@ namespace AnnoDesigner.Converters
             if (!(value is MouseAction mouseAction)) {
                 return null;
             }
-            var i = new ImageSourceConverter();
             switch (mouseAction)
             {
                 case MouseAction.LeftClick:
                 case MouseAction.LeftDoubleClick:
-                    //return i.ConvertFromInvariantString(RESOURCE_ROOT_PATH + "/left-click.png");
                     return RESOURCE_ROOT_PATH + "/left-click.png";
                 case MouseAction.RightClick:
                 case MouseAction.RightDoubleClick:
-                    //return i.ConvertFromInvariantString(RESOURCE_ROOT_PATH + "/right-click.png");
                     return RESOURCE_ROOT_PATH + "/right-click.png";
                 case MouseAction.MiddleClick:
                 case MouseAction.MiddleDoubleClick:
-                    //return i.ConvertFromInvariantString(RESOURCE_ROOT_PATH + "/middle-click.png");
                     return RESOURCE_ROOT_PATH + "/middle-click.png";
                 default:
                     return "";
