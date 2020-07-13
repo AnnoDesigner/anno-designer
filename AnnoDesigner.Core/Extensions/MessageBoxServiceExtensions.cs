c5b734b26a1a192c6fc2f884011a912775a50e19
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-07-13 14:49:57 +0200

    - add service API

---
+++ AnnoDesigner.Core/Extensions/MessageBoxServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using AnnoDesigner.Core.Services;
+
+namespace AnnoDesigner.Core.Extensions
+{
+    public static class MessageBoxServiceExtensions
+    {
+        public static void ShowMessage(this IMessageBoxService service, string message, string title)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            service.ShowMessage(null, message, title);
+        }
+
+        public static void ShowWarning(this IMessageBoxService service, string message, string title)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            service.ShowWarning(null, message, title);
+        }
+
+        public static void ShowError(this IMessageBoxService service, string message, string title)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            service.ShowError(null, message, title);
+        }
+
+        public static bool ShowQuestion(this IMessageBoxService service, string message, string title)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            return service.ShowQuestion(null, message, title);
+        }
+    }
+}
