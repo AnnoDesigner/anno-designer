fe2c1a28ad7df48ef49aaf05f7256713bf2d2882
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-07-16 11:36:47 +0200

    add comment about localization

--- AnnoDesigner/Models/UserDefinedColorType.cs
+++ AnnoDesigner/Models/UserDefinedColorType.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace AnnoDesigner.Models
 {
+    /// <summary>
+    /// Types of (predefined) colors.
+    /// </summary>
+    /// <remarks>The members are used as a translation key with a prefix of "ColorType" (e.g. ColorTypeLight).</remarks>
     public enum UserDefinedColorType
     {
         Default,
