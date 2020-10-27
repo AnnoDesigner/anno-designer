ada7d414de8e502f5b2b1b660d573b267ff8c751
FroggieFrog <FroggieFrog@users.noreply.github.com>
2020-10-27 09:41:23 +0100

    - update year of Copyright

--- AnnoDesigner.Core/Properties/AssemblyInfo.cs
+++ AnnoDesigner.Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AnnoDesigner.Core")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
