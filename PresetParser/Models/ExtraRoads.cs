ba5b0a26ffc36cd1c0b22fa70b1a5d20e06b0315
FroggieFrog <FroggieFrog@users.noreply.github.com>
2021-11-29 10:29:45 +0100

    - add tests for `GetBlockingTiles` - add tests for `GetExtraRoads`

--- PresetParser/Models/ExtraRoads.cs
+++ PresetParser/Models/ExtraRoads.cs
@@ -2,6 +2,16 @@
 {
     public class ExtraRoads : ExtraPreset
     {
+        public ExtraRoads()
+        {
+            Road = true;
+            Borderless = true;
+            Group = null;
+            Header = "- Road Presets";
+            IconFileName = null;
+            Template = "Road";
+        }
+
         public bool Borderless { get; set; }
         public bool Road { get; set; }
     }
