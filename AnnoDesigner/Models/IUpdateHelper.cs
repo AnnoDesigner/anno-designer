85486eca1548c8b80d75911ff887cddc4b55f6e9
FroggieFrog <FroggieFrog@users.noreply.github.com>
2021-12-03 15:04:10 +0100

    `UpdateHelper` supports pre-release of app (#382)
    * add support pre-release of app
    * display new version in preferences window

--- AnnoDesigner/Models/IUpdateHelper.cs
+++ AnnoDesigner/Models/IUpdateHelper.cs
@@ -14,6 +14,6 @@ namespace AnnoDesigner.Models
 
         Task ReplaceUpdatedPresetsFilesAsync();
 
-        Task<bool> IsNewAppVersionAvailableAsync();
+        Task<(bool, Version)> IsNewAppVersionAvailableAsync();
     }
 }
