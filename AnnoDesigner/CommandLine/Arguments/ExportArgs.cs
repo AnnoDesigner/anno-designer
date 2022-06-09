3b9f2c4d673e573088482d145742973af06b528d
FroggieFrog <FroggieFrog@users.noreply.github.com>
2022-06-09 16:25:17 +0200

    - added usage section to help output

--- AnnoDesigner/CommandLine/Arguments/ExportArgs.cs
+++ AnnoDesigner/CommandLine/Arguments/ExportArgs.cs
@@ -1,10 +1,23 @@
-﻿using CommandLine;
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
 
 namespace AnnoDesigner.CommandLine.Arguments
 {
     [Verb("export", HelpText = "Exports the specified layout file to an image and closes immediately")]
     public class ExportArgs : IProgramArgs
     {
+        [Usage]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Export with default settings", new ExportArgs { Filename = @"C:\path\to\layout\file.ad", ExportedFilename = @"C:\path\to\exported\image.png" });
+                yield return new Example("Export with rendered influences", new ExportArgs { Filename = @"C:\path\to\layout\file.ad", ExportedFilename = @"C:\path\to\exported\image.png", RenderInfluences = true });
+                yield return new Example("Export with statistics and version information", new ExportArgs { Filename = @"C:\path\to\layout\file.ad", ExportedFilename = @"C:\path\to\exported\image.png", RenderStatistics = true, RenderVersion = true });
+            }
+        }
+
         [Value(0, MetaName = "layoutPath", HelpText = "Path to layout file (*.ad).", Required = true)]
         public string Filename { get; set; }
 
@@ -14,7 +27,7 @@ namespace AnnoDesigner.CommandLine.Arguments
         [Option("border", Default = 1)]
         public int Border { get; set; }
 
-        [Option("gridSize", Default = 20, Max = Constants.GridStepMax, Min = Constants.GridStepMin)]
+        [Option("gridSize", Default = 20)]
         public int GridSize { get; set; }
 
         [Option("renderGrid", Default = true)]
--- AnnoDesigner/CommandLine/Arguments/OpenArgs.cs
+++ AnnoDesigner/CommandLine/Arguments/OpenArgs.cs
@@ -1,10 +1,21 @@
-﻿using CommandLine;
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
 
 namespace AnnoDesigner.CommandLine.Arguments
 {
     [Verb("open", HelpText = "Starts AnnoDesigner with specified layout file opened")]
     public class OpenArgs : IProgramArgs
     {
+        [Usage]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Open with layout", new OpenArgs { Filename = @"C:\path\to\layout\file.ad" });
+            }
+        }
+
         [Value(0, MetaName = "layoutPath", HelpText = "Path to layout file (*.ad).", Required = true)]
         public string Filename { get; set; }
     }
