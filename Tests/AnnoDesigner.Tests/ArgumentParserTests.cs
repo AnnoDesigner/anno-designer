4fb5ccb0fc3d92acf751792df2e8f6e819171260
FroggieFrog <FroggieFrog@users.noreply.github.com>
2022-06-09 12:24:50 +0200

    - fixed tests - tests can run in VS 2022

--- Tests/AnnoDesigner.Tests/ArgumentParserTests.cs
+++ Tests/AnnoDesigner.Tests/ArgumentParserTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using AnnoDesigner.CommandLine;
+using AnnoDesigner.CommandLine.Arguments;
 using Xunit;
 
 namespace AnnoDesigner.Tests
